72c0dcc205d4d77a735bf765e3ad344df22ae7d6
alveyE <gcbdalvey@gmail.com>
2024-02-28 15:28:58 -0700

    added service tests

--- server/src/test/java/passoffTests/serviceTests/GameServiceTest.java
+++ server/src/test/java/passoffTests/serviceTests/GameServiceTest.java
@@ -1,11 +1,22 @@
 package passoffTests.serviceTests;
 
 import service.GameService;
+import service.ResponseException;
 import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.*;
 
-
 import dataAccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 
 
 public class GameServiceTest {
@@ -22,6 +33,78 @@ public class GameServiceTest {
         userService.clear();
     }
 
+
+    @Test
+    void createGameSuccess() throws DataAccessException, ResponseException{
+        GameData game = new GameData(1, "a", "b", "game", null);
+        UserData user = new UserData("a", "b", "c");
+        AuthData auth = userService.register(user);
+        assertNotNull(auth);
+        assertEquals("a", auth.username());
+
+        int gameID = gameService.createGame(game, auth.authToken());
+        assertNotNull(gameID);
+    }
+
+    @Test
+    void createGameFail() {
+        GameData game = new GameData(1, "a", "b", "game", null);
+        assertThrows(ResponseException.class, () -> gameService.createGame(game, null));
+        assertThrows(ResponseException.class, () -> gameService.createGame(game, "invalid"));
+    }
+
+
+    @Test
+    void listGameSuccess() throws DataAccessException, ResponseException{
+        GameData game = new GameData(1, "a", "b", "game", null);
+        UserData user = new UserData("a", "b", "c");
+        AuthData auth = assertDoesNotThrow(() -> userService.register(user));
+        assertNotNull(auth);
+        assertEquals("a", auth.username());
+
+        int gameID = gameService.createGame(game, auth.authToken());
+        assertNotNull(gameID);
+
+        GameData game2 = new GameData(2, "a", "b", "game2", null);
+        int gameID2 = gameService.createGame(game2, auth.authToken());
+        assertNotNull(gameID2);
+
+        ArrayList<GameData> games = gameService.listGames(auth.authToken());
+        assertNotNull(games);
+        assertEquals(2, games.size());
+
+    }
+
+    @Test
+    void listGameFail() {
+        assertThrows(ResponseException.class, () -> gameService.listGames(null));
+        assertThrows(ResponseException.class, () -> gameService.listGames("invalid"));
+    }
+
+
+    @Test
+    void joinGameSuccess(){
+        GameData game = new GameData(1, "a", "b", "game", null);
+        UserData user = new UserData("a", "b", "c");
+        AuthData auth = assertDoesNotThrow(() -> userService.register(user));
+        assertNotNull(auth);
+        assertEquals("a", auth.username());
+
+        int gameID = assertDoesNotThrow(() -> gameService.createGame(game, auth.authToken()));
+        assertNotNull(gameID);
+
+        assertDoesNotThrow(() -> gameService.joinGame(gameID, auth.authToken(), "WHITE"));
+    }
+
+    @Test
+    void joinGameFail() {
+        assertThrows(ResponseException.class, () -> gameService.joinGame(1, null, "WHITE"));
+        assertThrows(ResponseException.class, () -> gameService.joinGame(1, "invalid", "WHITE"));
+        assertThrows(ResponseException.class, () -> gameService.joinGame(1, "valid", "invalid"));
+    }
+    
+
+    
 
 
 
--- server/src/test/java/passoffTests/serviceTests/UserServiceTest.java
+++ server/src/test/java/passoffTests/serviceTests/UserServiceTest.java
@@ -1,8 +1,15 @@
 package passoffTests.serviceTests;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.*;
 
 
 import dataAccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import service.ResponseException;
 import service.UserService;
 
 public class UserServiceTest {
@@ -15,8 +22,50 @@ public class UserServiceTest {
     }
 
     @Test
-    void testClear() throws DataAccessException{
+    void registerSuccess() throws DataAccessException, ResponseException {
+        UserData user = new UserData("a", "b", "c");
+        AuthData auth = userService.register(user);
+        assertNotNull(auth);
+        assertEquals("a", auth.username());
+    }
+
+    @Test
+    void registerFail() {
+        UserData user = new UserData("a", "b", "c");
+        assertThrows(ResponseException.class, () -> userService.register(user));
+    }
+
+    @Test
+    void loginSuccess() throws DataAccessException, ResponseException {
+        UserData user = new UserData("newUser", "b", "email.com");
+        userService.register(user);
+        AuthData auth = userService.login(user);
+        assertNotNull(auth);
+        assertEquals("newUser", auth.username());
+    }
+
+    @Test
+    void loginFail() {
+        UserData user = new UserData("", null, "c");
+        assertThrows(ResponseException.class, () -> userService.login(user));
+    }
+
+    @Test
+    void logoutSuccess() throws DataAccessException, ResponseException {
+        UserData user = new UserData("a", "b", "c");
+        AuthData auth = userService.register(user);
+        userService.logout(auth.authToken());
+    }
+
+    @Test
+    void logoutFail() {
+        assertThrows(ResponseException.class, () -> userService.logout(null));
+    }
+
+    @Test
+    void clearSuccess() throws DataAccessException {
         userService.clear();
     }
+
 
 }
