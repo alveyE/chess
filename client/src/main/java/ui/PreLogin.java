6f23b372c1978a278f293ff226e61e09a6ecdf1e
alveyE <gcbdalvey@gmail.com>
2024-03-23 11:22:48 -0600

    pre login commands

--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -1,4 +1,43 @@
 package ui;
 
 public class PreLogin {
+
+    private final String url;
+    public ServerFacade serverFacade;
+
+    public PreLogin(String url) {
+        this.url = url;
+        serverFacade = new ServerFacade(url);
+    }
+
+
+    public String runCommand(String command){
+        String[] commandParts = command.toLowerCase().split(" ");
+        String cmd = commandParts[0];
+        String[] args = new String[commandParts.length - 1];
+        for (int i = 1; i < commandParts.length; i++) {
+            args[i - 1] = commandParts[i];
+        }
+        switch (cmd) {
+            case "register":
+                if (args.length != 3) {
+                    return "Usage: register <username> <password> <email>";
+                }
+                return serverFacade.register(args[0], args[1], args[2]).toString();
+            case "login":
+                if (args.length != 2) {
+                    return "Usage: login <username> <password>";
+                }
+                return serverFacade.login(args[0], args[1]).toString();
+            case "quit":
+                return "Goodbye!";
+            default:
+                return "register <username> <password> <email> - to create an account\nlogin <username> <password> - to play chess\nquit - playing chess\nhelp - with possible commands";
+        }
+
+
+        
+    }
+
+
 }
