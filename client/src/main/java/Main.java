fc726499c946c70cf83b307053785177d6b7f2e6
alveyE <gcbdalvey@gmail.com>
2024-03-25 15:43:32 -0600

    client ui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,37 @@
-import chess.*;
+import java.util.Scanner;
+
+import ui.PostLogin;
+import ui.PreLogin;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String url = "http://localhost:8080";
+        if (args.length == 1) {
+            url = args[0];
+        }
+        PreLogin preLogin = new PreLogin(url);
+        PostLogin postLogin = new PostLogin(url);
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        boolean isLoggedIn = false;
+        while (!command.equals("quit")) {
+            System.out.println("Enter a command:");
+            command = scanner.nextLine();
+            if (!isLoggedIn) {
+                String response = preLogin.runCommand(command);
+                System.out.println(response);
+                if (response.startsWith("Successfully logged in")) {
+                    isLoggedIn = true;
+                }
+            } else if (isLoggedIn ) {
+                String response = postLogin.runCommand(command);
+                System.out.println(response);
+                if (response.startsWith("Successfully logged out")) {
+                    isLoggedIn = false;
+                }
+            } 
+        }
+        scanner.close();
+        
     }
 }
